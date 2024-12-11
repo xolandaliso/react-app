9501f8b6dcf356b3719ebfcc806d73ad1d8c71bf
xola <xola.ndaliso@gmail.com>
2024-12-11 16:09:14 +0200

    added image holders in this script

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import '../components/css/landingpage.css'; // Ensure correct path to your CSS file
+import './css/landingpage.css'; // Assuming you'll use custom CSS for styling
+import weld from './images/weld.png';
+
 
 function AboutUs() {
   return (
     <div className="container services-container">
       <h2 className="services-title">ABOUT US</h2>
-      <div className="row mt-4">
-        <div className="col-md-6">
+      
+      {/* Mission Section */}
+      <div className="about-section">
+        <div className="about-text">
           <h4>Mission</h4>
           <p>
             To provide exceptional quality and innovative metalwork solutions
@@ -14,7 +18,17 @@ function AboutUs() {
             sustainability and integrity in all our operations.
           </p>
         </div>
-        <div className="col-md-6">
+        <div className="about-image">
+          <img src={ weld } alt="Mission" />
+        </div>
+      </div>
+
+      {/* Vision Section */}
+      <div className="about-section">
+        <div className="about-image">
+          <img src={ weld } alt="Vision" />
+        </div>
+        <div className="about-text">
           <h4>Vision</h4>
           <p>
             To be the leading provider of premium metalwork solutions in
