f5dc8c2df3302923371081743107d8b6db9b0dc1
xola <xola.ndaliso@gmail.com>
2024-12-11 13:38:47 +0200

    h1 ---> h2 on the about us page

--- src/components/serviceslist.js
+++ src/components/serviceslist.js
@@ -5,7 +5,7 @@ import '../components/css/landingpage.css';
 function ServicesList() {
   return (
     <div id="services-section" className="container services-container">
-        <h1 className="services-title">SERVICES RENDERED</h1>
+        <h2 className="services-title">SERVICES RENDERED</h2>
         <div className="row">
             <div className="col-md-3">
             <h4>Aluminium Windows & Doors</h4>
@@ -23,6 +23,17 @@ function ServicesList() {
             <h4>Installations</h4>
             <p>We specialize in the installation of various metal and glass structures, ensuring durability and quality.</p>
             </div>
+        </div>
+        <div className="row" style={{ marginTop: '50px' }}>
+            <div className="col-md-3">
+            <h4>Aluminium Windows & Doors</h4>
+            <p>We provide high-quality aluminium windows and doors for both residential and commercial properties.</p>
+            </div>
+            <div className="col-md-3">
+            <h4>Shower Frames</h4>
+            <p>Our custom shower frames are designed to provide both style and functionality for your bathroom.</p>
+            </div>
+          
         </div>
     </div>
   );
