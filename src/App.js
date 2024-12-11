2d54e355186b702e96ff086d008303563274f61e
xola <xola.ndaliso@gmail.com>
2024-12-11 17:18:16 +0200

    added links to the app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/layout';
 import Home from './pages/home';
-import ServicesPage from './pages/services';
+import ServicesPage from './pages/servicespage';
 import ContactForm from './components/contactform';
 import AboutUs from './components/aboutus';
 //import Navbar from './components/navbar';
