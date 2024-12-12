11f9c9e16b48b5995abb2b74cde6e9b4a65a579b
xola <xola.ndaliso@gmail.com>
2024-12-12 08:41:09 +0200

    hide footer upon landing - only show when scrolling

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './navbar';
 import Footer from './footer';
 
 const Layout = () => {
+  const [showFooter, setShowFooter] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollHeight = window.pageYOffset;
+      setShowFooter(scrollHeight > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <>
       <Navbar />
       <div>
         <Outlet />
       </div>
-       <Footer />  /*Footer will be displayed at the bottom of every page*/ 
+      {showFooter && <Footer />}
     </>
   );
 };
