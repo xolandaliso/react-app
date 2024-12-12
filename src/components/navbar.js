f0bf0a81f96dd71e897c68df1f16b71485e26093
xola <xola.ndaliso@gmail.com>
2024-12-12 08:40:25 +0200

    fixed broken hamburger navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS
 import './css/navbar.css'; // Add your custom styles here
 import logo from './images/window.png';
 import { Link } from 'react-router';
@@ -7,56 +8,55 @@ import { Link } from 'react-router';
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-  <div className="container">
-    {/* Logo and Brand Text */}
-    <a className="navbar-brand" href="/">
-      <img
-        src={logo}
-        alt="Logo"
-        className="navbar-logo"
-      />
-      <span>DR's Aluminium +</span>
-    </a>
+      <div className="container">
+        {/* Logo and Brand Text */}
+        <a className="navbar-brand" href="/">
+          <img
+            src={logo}
+            alt="Logo"
+            className="navbar-logo"
+          />
+          <span>DR's Aluminium +</span>
+        </a>
 
-    {/* Navbar Links */}
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav ms-auto">
-        <li className="nav-item">
-          <Link className="nav-link enlarged-link" to="/">
-            HOME
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link enlarged-link" to="/about">
-            ABOUT
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link enlarged-link" to="/services">
-            SERVICES
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link enlarged-link" to="/contact">
-            CONTACT US
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
+        {/* Navbar Links */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link enlarged-link" to="/">
+                HOME
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link enlarged-link" to="/about">
+                ABOUT
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link enlarged-link" to="/services">
+                SERVICES
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link enlarged-link" to="/contact">
+                CONTACT US
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
